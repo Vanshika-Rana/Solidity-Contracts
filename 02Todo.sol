13963ecb929804a9cf3be7667fa962b060de05ea
Vanshika Rana <ranavanshika172000@gmail.com>
2022-03-22 01:40:19 +0530

    Created Todo Smart Contract

---
+++ 02Todo.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract Todo{
+     mapping(address => string[]) todos;
+
+    // adding todos 
+    function addTodo(string memory _todo) public{
+        todos[msg.sender].push(_todo);
+    }
+    // deleting todos: This just deletes the element and doesn't repositions 
+    function delTodo(uint idx) public{
+        require(idx < todos[msg.sender].length);
+        delete todos[msg.sender][idx];
+    }
+    // getting todos
+    function getTodo() public view returns(string[] memory){
+        return(todos[msg.sender]);
+    }
+}
